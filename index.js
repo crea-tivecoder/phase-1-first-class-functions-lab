8e4537f3cd646b08f376f6e17e93cee06fd6235e
crea-tivecoder <nyawirawaweru.com>
2024-07-04 13:31:32 +0300

    Add first-class-functions-lab

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'No'];
+console.log(returnFirstTwoDrivers(drivers));
+
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);
+};
+
+
+//returnLastTwoDrivers
+const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2);
+};
+
+//selectingDrivers
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+//createFareMultiplier
+const createFareMultiplier = function(multiplier){
+    return function (fare) {
+        return fare * multiplier;
+    };
+};
+
+//fareDoubler
+const fareDoubler = createFareMultiplier (2)
+
+//fareTripler
+const fareTripler = createFareMultiplier (3)
+
+//selectDifferentDrivers
+function selectDifferentDrivers(arrayOfDrivers, func){
+    if (typeof func ==='function') {
+        return func (arrayOfDrivers);
+
+    } else {
+        throw new Error ('Second argument must be a function');
+    }
+}
